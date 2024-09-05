07d568c0647c21dda9f9d4b4ac8c855825b4b574
CaniShen <164502320+CaniShen@users.noreply.github.com>
2024-09-05 10:07:21 +0200

    proyecto que deberia subir 04-09-2024(clase)

---
+++ 13_microservicio_buscador/src/main/java/controller/BuscadorController.java
@@ -0,0 +1,38 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import model.ResultadoDto;
+import service.BuscadorService;
+
+@RestController
+public class BuscadorController {
+	@Autowired
+	@Qualifier("buscadorEstandar")
+	BuscadorService buscadorService;
+
+	public BuscadorController(BuscadorService buscadorService) {
+		super();
+		this.buscadorService = buscadorService;
+	}
+	@PostMapping(value="alta",produces = MediaType.TEXT_PLAIN_VALUE,consumes = MediaType.APPLICATION_JSON_VALUE)
+	public String alta (@RequestBody ResultadoDto resultado) {
+		return String.valueOf(buscadorService.agregar(resultado));
+	}
+	@GetMapping(value="/buscar/{tematica}",produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<ResultadoDto> buscarPorTematica(@PathVariable("tematica")String tematica) {
+	
+		return buscadorService.buscar(tematica);
+	
+	}
+
+}
